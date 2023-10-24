5289c007e80b988fe2b052a1c88c18af51b8160c
Евгений Чайка <chay67ka26@gmail.com>
2023-10-24 14:21:25 +0300

    update views.index to HomePageView(TemplateView) class, update article.views.index to IndexView(View) class

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'app_name': 'hexlet_django_blog.articles'
-    })
+class IndexView(View):
+
+    def get(self, request, **kwargs):
+        return render(request, 'articles/index.html', context={
+            'app_name': 'hexlet_django_blog.articles'
+        })
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.HomePageView.as_view()),
     path('about/', views.about),
     path('admin/', admin.site.urls),
     path('articles/', include('hexlet_django_blog.article.urls')),
